c6531f422d189b774ba2949f231e510430284f92
Xudayberganov <xudayberganovbackend@gmail.com>
2025-07-11 10:37:05 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -158,4 +158,9 @@ async def main():
 
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(main())
+    import sys
+    if sys.platform.startswith("win"):
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+    nest_asyncio.apply()
+    asyncio.run(main())
